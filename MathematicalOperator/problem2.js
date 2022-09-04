344a68081c1a83161f2179b99d3c9c7100bf6391
Nitish12345678 <bp008025@gmail.com>
2022-09-04 15:40:42 +0530

    Mathematical_Operator

---
+++ MathematicalOperator/problem2.js
@@ -0,0 +1,4 @@
+var one = 1, two = 2, three = 3, four = 4, five = 5, six = 6;
+two = 2 * 2, five = 5 * 2, six = 6 * 2;
+three = 3 * 3, four = 4 * 3;
+console.log(one + two + three + four + five + six);
